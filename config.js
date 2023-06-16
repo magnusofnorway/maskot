a4644a27b5fdbf01e3bdfd79e11ced834cfbb23d
magnusofnorway <124913333+magnusofnorway@users.noreply.github.com>
2023-06-16 18:19:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-window.botpressWebChat.init({
+window.maskotWebChat.init({
     "botId": "a23a213c-4dc2-4ab0-b7e4-b8b17d01818c",
-    "hostUrl": "https://cdn.botpress.cloud/webchat/v0",
-    "messagingUrl": "https://messaging.botpress.cloud",
+    "hostUrl": "https://aimagnus.com/maskot/v0",
+    "messagingUrl": "https://aimagnus.com/messaging",
     "clientId": "a23a213c-4dc2-4ab0-b7e4-b8b17d01818c"
 });
