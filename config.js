87b5e09cae85057e2ce0c8ead8aefb5209c72160
magnusofnorway <124913333+magnusofnorway@users.noreply.github.com>
2023-06-16 18:07:53 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.botpressWebChat.init({
+    "botId": "a23a213c-4dc2-4ab0-b7e4-b8b17d01818c",
+    "hostUrl": "https://cdn.botpress.cloud/webchat/v0",
+    "messagingUrl": "https://messaging.botpress.cloud",
+    "clientId": "a23a213c-4dc2-4ab0-b7e4-b8b17d01818c"
+});
